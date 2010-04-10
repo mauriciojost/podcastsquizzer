4b550c6aae59903aa2393de4220ce805f0249e08
mauriciojost <mauriciojost@3f742c90-793b-2ff0-923e-e2e6f1652267>
2010-04-10 11:16:14 +0000

    Changing a little bit the class Tuple.

--- j2me/persistLibrary/src/persistencepackage/Tuple.java
+++ j2me/persistLibrary/src/persistencepackage/Tuple.java
@@ -7,20 +7,17 @@ package persistencepackage;
 
 
 public class Tuple {
-    private String key;
-    private String value;
-    private String extra;
+    private String[] tuple;
     
     public Tuple (String key, String value){
-        this.key = key;
-        this.value = value;
-        this.extra = "";
+        this(key, value, "");
     }
     
     public Tuple (String key, String value, String extra){
-        this.key = key;
-        this.value = value;
-        this.extra = extra;
+        tuple = new String[3];
+        tuple[0] = key;
+        tuple[1] = value;
+        tuple[2] = extra;
     }
     
     private void reeplaceChars(String str, String newc, String oldc){
@@ -30,6 +27,7 @@ public class Tuple {
         /* En el caso de utilizar separadores de más de un caracter traería problemas... */
     }
     
+    
     private boolean hasNoData(String a){
         if (a==null) 
             return true;
@@ -38,48 +36,48 @@ public class Tuple {
     }
     
     public void setExtra(String a){
-        this.extra = a;
+        tuple[2] = a;
     }
     public void setValue(String a){
-        this.value = a;
+        tuple[1] = a;
     }
     public void setKey(String a){
-        this.key = a;
+        tuple[0] = a;
     }
-    public String getExtra(){return extra;}
-    public String getValue(){return value;}
-    public String getKey(){return key;}
+    public String getExtra(){return tuple[2];}
+    public String getValue(){return tuple[1];}
+    public String getKey(){return tuple[0];}
     public String getString(String vseparator, String lseparator){
         String vseparator2 = " " + vseparator + " ";
         
         
         
-        reeplaceChars(extra, vseparator, lseparator);
-        reeplaceChars(key, vseparator, lseparator);
-        reeplaceChars(value, vseparator, lseparator);
+        reeplaceChars(tuple[2], vseparator, lseparator);
+        reeplaceChars(tuple[0], vseparator, lseparator);
+        reeplaceChars(tuple[1], vseparator, lseparator);
         
-        if (hasNoData(extra)) {
-            if (hasNoData(value)) {
-                if (hasNoData(key)){
+        if (hasNoData(tuple[2])) {
+            if (hasNoData(tuple[1])) {
+                if (hasNoData(tuple[0])){
                     return "" + lseparator;
                 }else{
-                    return key + lseparator;
+                    return tuple[0] + lseparator;
                 }
             }else{
-                return key + vseparator2 + value + lseparator;
+                return tuple[0] + vseparator2 + tuple[1] + lseparator;
             }
         } else { 
-            return key + vseparator2 + value + vseparator2 + extra + lseparator;
+            return tuple[0] + vseparator2 + tuple[1] + vseparator2 + tuple[2] + lseparator;
         }
     }
     
     public Tuple getACopy(){
-        return new Tuple(new String(this.key), new String(this.value), new String(this.extra));
+        return new Tuple(new String(getKey()), new String(getValue()), new String(getExtra()));
     }
     public void copyTheGivenTuple(Tuple t){
-        this.key = new String(t.getKey());
-        this.value = new String(t.getValue());
-        this.extra = new String(t.getExtra());
+        this.setKey(new String(t.getKey()));
+        this.setValue(new String(t.getValue()));
+        this.setExtra(new String(t.getExtra()));
     }
     
     public boolean belongsToGroup(String group_id){
